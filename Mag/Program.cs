f2177851ad0004ab6c5d8c43da0e3677f3f07fff
Eryczek11 <46167722+Eryczek11@users.noreply.github.com>
2020-09-02 08:49:42 +0200

    Dodaj pliki projektów.

---
+++ Mag/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            foreach (var item in listOfAllBooks)
+            {
+                Console.WriteLine(item.title);
+                Console.WriteLine(item.author);
+                Console.WriteLine(item.pages);
+                Console.WriteLine("----------------------");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
